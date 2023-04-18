e2e6771b4b3da7a339b750be8ee476d3d551dc18
Brian Karmo <briankarmo@hotmail.com>
2023-04-17 23:33:59 -0400

    Add files via upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,13 +14,13 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-        My journey as a developer began with a passion for web development and design, which eventually evolved into a love for coding and problem-solving. With an extensive range of projects under my belt, I have acquired substantial expertise in working with popular frameworks like React.js, Next.js, and Redux, as well as utilizing Node.js for building server-side applications. My development arsenal also includes several libraries and styling tools like Axios for HTTP clients and CSS preprocessors via TailwindCSS, to create dynamic, interactive, and responsive component-based applications. In addition, I have honed my version control skills by using GitHub, which helps me effectively manage my codebase.  Furthemore, I have also employed APIs, which enable seamless integration with external data sources and services, to make my applications even more interactive.
+        My journey as a developer began with a passion for web development and design, which eventually evolved into a love for coding and problem-solving. With an extensive range of projects under my belt, I have acquired substantial expertise in working with popular frameworks like React.js, Next.js, and Redux, as well as utilizing Node.js for building server-side applications. My development arsenal also includes several libraries and styling tools like Axios for HTTP clients and CSS preprocessors via TailwindCSS, to create dynamic, interactive, and responsive component-based applications. In addition, I have honed my version control skills by using GitHub, which helps me effectively manage my codebase.  Furthemore, I have employed APIs, which enable seamless integration with external data sources and services, to make my applications even more interactive.
         </p>
 
         <br />
 
         <p className="text-xl">
-        "As an enthusiastic learner, I am continually driven to enhance my expertise in JavaScript development. My focus spans across various aspects of front-end software engineering, including frameworks, object-oriented programming, UI/UX development, and DOM manipulation. My relentlessness and dedication to solving complex problems have helped me develop the resilience required to overcome any obstacle. Coupled with my passion for staying on the cutting edge of technology and my unwavering commitment to constant improvement, set me apart as an exceptional problem solver."
+        "As a dedicated and enthusiastic learner, I am continually driven to enhance and broaden my expertise in JavaScript development. My focus encompasses various aspects of front-end software engineering, including frameworks, object-oriented programming, UI/UX development, and DOM manipulation. Through my relentless pursuit of problem-solving and unwavering commitment to constant improvement, I have developed the resilience and dedication required to overcome any obstacle. This, coupled with my passion for staying on the cutting edge of technology, sets me apart as an exceptional problem solver.""
         </p>
       </div>
     </div>
